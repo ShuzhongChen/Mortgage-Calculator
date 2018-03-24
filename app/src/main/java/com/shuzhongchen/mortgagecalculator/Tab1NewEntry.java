aec112e15a976be82a9b3f2e1fa60ca8c4841640
“ShuzhongChen” <chenshuzhongs@gmail.com>
2018-03-24 15:50:27 -0700

    initial

---
+++ app/src/main/java/com/shuzhongchen/mortgagecalculator/Tab1NewEntry.java
@@ -0,0 +1,22 @@
+package com.shuzhongchen.mortgagecalculator;
+
+/**
+ * Created by shuzhongchen on 3/24/18.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1NewEntry extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1newentry, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/shuzhongchen/mortgagecalculator/Tab2Map.java
@@ -0,0 +1,8 @@
+package com.shuzhongchen.mortgagecalculator;
+
+/**
+ * Created by shuzhongchen on 3/24/18.
+ */
+
+public class Tab2Map {
+}
