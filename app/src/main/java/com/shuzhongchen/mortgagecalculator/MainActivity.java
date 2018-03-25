4fd3de4e9d61de8245a4c05336566d722f85b182
“Yingran” <syrhuang@gmail.com>
2018-03-24 19:16:57 -0700

    Add google map

--- app/src/main/java/com/shuzhongchen/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/shuzhongchen/mortgagecalculator/MainActivity.java
@@ -1,7 +1,6 @@
 package com.shuzhongchen.mortgagecalculator;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -55,16 +54,6 @@ public class MainActivity extends AppCompatActivity {
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
